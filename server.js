efe8bc28ba295bf6e01d1b555a8de2285d675696
oranje322 <oranje322@mail.ru>
2020-10-02 01:10:06 +0300

    to deploy

--- server.js
+++ server.js
@@ -70,3 +70,21 @@ server.listen(9999, (err) => {
     }
     console.log('Сервер запущен!');
 });
+
+
+
+// const jsonServer = require('json-server');
+// const server = jsonServer.create();
+// const router = jsonServer.router('./public/database.json');
+// const middlewares = jsonServer.defaults({
+//     static: './build',
+// });
+//
+// const PORT = process.env.PORT || 3001;
+//
+// server.use(middlewares);
+// server.use(router);
+//
+// server.listen(PORT, () => {
+//     console.log('Server is running');
+// });
