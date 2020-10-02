c8cf8afb70cb7599d90fb3dda2615187b932123e
oranje322 <oranje322@mail.ru>
2020-10-03 02:06:17 +0300

    to deploy

--- src/socket.js
+++ src/socket.js
@@ -2,6 +2,6 @@ import io from 'socket.io-client'
 
 const PORT = process.env.PORT || 5000
 
-const socket = io(`http://localhost:${PORT}`);
+const socket = io(`https://reactt-mini-chat.herokuapp.com/:${PORT}`);
 
 export default socket;
